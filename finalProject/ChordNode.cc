9cb1df4cdfe4ca9f0f0655086c3c69b82eaba773
Abhishek Chandra <abhishek.chandra@yale.edu>
2014-12-11 16:08:35 -0500

    find successors/predecessors

--- finalProject/ChordNode.cc
+++ finalProject/ChordNode.cc
@@ -3,6 +3,9 @@
 #include <QApplication>
 #include <QDateTime>
 #include <QBitArray>
+
+#define JOIN_KEY "Join"
+#define isIn(a,b,c) (a>=b && a<c)
 ChordNode::ChordNode(){
 	setWindowTitle("Chordster");
 
@@ -25,7 +28,13 @@ ChordNode::ChordNode(){
 		exit(1);
 	//Create identifier for node
 	setCreateChordKey();
+	finger = new fingerTableEntry[idlength];
+	//Register a callback on NetSocket's readyRead signal to
+	//read pending datagrams.
+	// connect(&sock, SIGNAL(readyRead()),this, SLOT(gotNewMessage()));
+	
 	//Dialog to connect to a chord node
+
 	//Here we need preserve join conditions from 4.4
 	//For now:
 	//	locate succ and pred of succ
@@ -42,7 +51,8 @@ void ChordNode::setCreateChordKey(){
 	QHostAddress address = sock.hostInfo->addresses()[0];
 	quint16 port = sock.myPort;
 	QString nodeName = address.toString() + ":" + QString::number(port);
-  nodeKey = getKeyFromName(nodeName);
+  quint32 nodeKey = getKeyFromName(nodeName);
+  selfNode = new Node(address,port,nodeKey);
   // nodeIdentifier = (QCA::Hash("sha1").hashToString(byteArray));
 
 }
@@ -60,10 +70,89 @@ quint32 ChordNode::getKeyFromName(QString name){
 	}
 	//Create appropriately sized int
 	qint32 x = 0;
-	for (int i=0;i<idlength;++i){
+	for (uint i=0;i<idlength;++i){
 		x<<=1; 			//left shift by one place
 		x|=bits[i];	//copy next bit
 	}
 	//modulo identifier circle size
 	return x%(1<<idlength);
+}
+void ChordNode::sendJoinRequest(QHostAddress host, quint16 port){
+	QVariantMap message;
+	message.insert(JOIN_KEY, selfNode->key);
+	writeToSocket(message,port,host);
+}
+void ChordNode::writeToSocket(QVariantMap message, quint16 port,QHostAddress host)
+{
+	QByteArray data = serializeToByteArray(message);
+	sock.writeDatagram(data,host,port);	
+}
+
+QByteArray ChordNode::serializeToByteArray(QVariantMap message)
+{
+	QByteArray data;
+	QDataStream* stream = new QDataStream(&data, QIODevice::WriteOnly);
+	(*stream) << message;
+	delete stream;
+	return data; 
+}
+
+QVariantMap ChordNode::readDeserializeDatagram(QHostAddress* sender, quint16* senderport){
+	//Read Datagram(as a serialized QByteArray) and sender details 
+	QByteArray *serializedarr = new QByteArray();
+	serializedarr->resize(sock.pendingDatagramSize());
+	sock.readDatagram(serializedarr ->data(),serializedarr->size(),
+										sender, senderport);
+
+	//Deserialize the Datagram to a QVMap
+	QDataStream instream(serializedarr,QIODevice::ReadOnly);
+	QVariantMap map;
+	instream >> map;
+	delete serializedarr;
+	return map;	
+}
+
+void ChordNode::gotNewMessage(){
+	//Read Datagram(as a serialized QByteArray) and retrieve sender details
+	QHostAddress sender; quint16 port; 
+	QVariantMap map = readDeserializeDatagram(&sender,&port);
+
+	if (map.contains(JOIN_KEY))
+		handleJoinRequest(map.value(JOIN_KEY).toString(),sender,port);
+}
+
+void ChordNode::handleJoinRequest(QString key,QHostAddress sender,quint16 port){
+
+}
+
+void ChordNode::InitializeIdentifierCircle(){
+	for (uint i=0; i<idlength;++i){
+		finger[i].start = (selfNode->key + (1<<i))% idSpaceSize;
+		finger[i].end = (selfNode->key + (1<<(i+1)))% idSpaceSize;
+		finger[i].node = selfNode;
+	}
+	//update successor
+	selfNode->successor = finger[1].node;
+	//update predecessor
+	selfNode->predecessor = selfNode; 
+}
+
+Node* ChordNode::findSuccessor(quint32 key){
+	Node* n = findPredecessor(key); //Use this to store predecessor
+	return n->successor;
+}
+
+Node* ChordNode::findPredecessor(quint32 key){
+	Node* n = selfNode;
+	while(!isIn(key,n->key,n->successor->key))
+		n=closestPrecedingFinger(key);
+	return n; 
+}
+
+Node* ChordNode::closestPrecedingFinger(quint32 key){
+	for(int i=CHORD_BITS-1;i>=0;--i){
+		if(isIn(finger[i].node->key,selfNode->key,key))
+			return finger[i].node;
+	}
+	return selfNode;
 }
--- finalProject/ChordNode.hh
+++ finalProject/ChordNode.hh
@@ -31,6 +31,33 @@ A basic Peerster implementation
 #include <QListWidget>
 
 #define CHORD_BITS 16
+class Node
+{
+public:
+	Node(QHostAddress a,quint16 p,quint32 k,Node* s=0,Node* pr=0){
+		address = a; port = p; key = k;
+		successor = s; predecessor = pr;
+	}
+	Node(const Node &n){
+		address = n.address;port = n.port; key = n.key;
+		successor=n.successor; predecessor=n.predecessor;
+	}
+	QHostAddress address;
+	quint16 port;
+	quint32 key;
+	Node* predecessor;
+	Node* successor;
+};
+
+class fingerTableEntry
+{
+public:
+	//Refer Table 1 of Chord paper
+	quint32 start;	//inclusive
+	quint32 end;		//exclusive
+	Node* node;
+};
+
 //Main class for chordster instance
 class ChordNode : public QDialog
 {
@@ -40,18 +67,31 @@ public:
 	ChordNode();
 
 public slots:
-
+	void gotNewMessage();					//Handles readyRead() for incoming datagrams
+	
 private:
-		static const quint32 idlength = CHORD_BITS;
-		NetSocket sock;								//Netsocket instance to bind to a port
-		
-		quint32 nodeKey;							//chord key created from node ip and port
-		QVBoxLayout *layout1;					//Layout of GUI
-		QPushButton* sharefilebutton;	//Button to launch dialog for file sharing
-		QPushButton* downloadfilebutton;//Launch dialog for file download	
+	static const quint32 idlength = CHORD_BITS;
+	static const quint32 idSpaceSize = 1<<idlength;
+	NetSocket sock;								//Netsocket instance to bind to a port
+	Node* selfNode;
+	fingerTableEntry* finger;
+	QVBoxLayout *layout1;					//Layout of GUI
+	QPushButton* sharefilebutton;	//Button to launch dialog for file sharing
+	QPushButton* downloadfilebutton;//Launch dialog for file download	
 
-		void setCreateChordKey();			//Create and store the chord key for node
-		quint32 getKeyFromName(QString);	//Create chord key from given QString
+	void InitializeIdentifierCircle();//Init a new id circle
+	quint32 getKeyFromName(QString);	//Create chord key from given QString		
+	void setCreateChordKey();			//Create and store the chord key for node
+	void sendJoinRequest(QHostAddress, quint16); // Send request to join chord
+	void writeToSocket(QVariantMap, quint16,QHostAddress);//Calls writeDatagram
+	QByteArray serializeToByteArray(QVariantMap);	//Serialze QVMap to QByteArray
+	QVariantMap readDeserializeDatagram(QHostAddress*, quint16*);
+	void handleJoinRequest(QString,QHostAddress,quint16);
+	Node* findSuccessor(quint32);
+	Node* findPredecessor(quint32);
+	Node* closestPrecedingFinger(quint32);
+
+
 };
 
 #endif // CHORDSTER_MAIN_HH
